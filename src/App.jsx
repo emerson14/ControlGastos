b3ed53b1aa704e3a8c7ae53cf59d976a044bfa5e
Emerson Guerrero DEV <emerson.guerrero@marketmix.com.co>
2024-09-24 11:14:03 -0500

    Se crea algunos componenetes y se arregla el context y se da estructura al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { GlobalProvider } from "./context/GlobalState"
-import Header from './components/Header'
-import Balance from "./components/Balance"
-import TransaccionForm from "./components/TransaccionForm"
+import { Balance } from "./components/Balance";
+import { ExpenseChart } from "./components/ExpenseChart";
+import { Header } from "./components/Header";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionForm } from "./components/transactions/TransaccionForm";
+import { TransactionList } from "./components/transactions/TransactionList";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
     <GlobalProvider>
-      <Header />
-      <Balance />
-      <TransaccionForm />
-      <h1>Hello World</h1>
+      <div className="bg-neutral-950 text-white h-screen flex justify-center items-center">
+        <div className="w-2/5 flex justify-center items-center">
+          <div className="bg-neutral-800 p-10 rounded-md w-full">
+            <Header />
+            <div className="flex flex-col md:flex-row justify-between gap-4">
+              <div className="flex-1">
+                <IncomeExpenses />
+                <Balance />
+                <TransactionForm />
+              </div>
+              <div className="flex-1 flex flex-col">
+                <ExpenseChart />
+                <TransactionList />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </GlobalProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,11 +1,50 @@
-import { createContext, useContext } from "react";
+import { useContext, useReducer, createContext, useEffect } from "react";
+import AppReducer from "./AppReducer";
 
-export const Context = createContext();
-export const useGlobalState = () =>{
-    const context = useContext(Context);
-    return context;
-}
+const initialState = {
+  transactions: [],
+};
+
+export const Context = createContext(initialState);
+
+export const useGlobalState = () => {
+  const context = useContext(Context);
+  if (!context)
+    throw new Error("useGlobalState must be used within a GlobalState");
+  return context;
+};
 
 export const GlobalProvider = ({ children }) => {
-  return <Context.Provider value={{ total: 100 }}>{children}</Context.Provider>;
+  const [state, dispatch] = useReducer(AppReducer, initialState, () => {
+    const localData = localStorage.getItem("transactions");
+    return localData ? JSON.parse(localData) : initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("transactions", JSON.stringify(state));
+  }, [state]);
+
+  const deleteTransaction = (id) =>
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+
+  const addTransaction = (transaction) =>
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+
+  return (
+    <Context.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 };
